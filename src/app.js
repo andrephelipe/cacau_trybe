628c8354f65b3ea0634cc19797472e794189645c
andrephelipe <andre.phelipe.1999@gmail.com>
2023-06-16 10:08:02 -0300

    criando o metodo get para os chocolates

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const { readChocolates } = require('./utils/fsUtils');
 
 const app = express();
 app.use(express.json());
+
+app.get('/', async (req, res) => {
+  const resultChocolates = await readChocolates();
+
+  res.status(200).json(resultChocolates);
+});
 
 module.exports = {
   app,
--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -2,3 +2,17 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const CHOCOLATES = '../../data/data.json';
+
+const readChocolates = async () => {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, CHOCOLATES));
+    const chocolates = JSON.parse(data);
+    return chocolates;
+  } catch (error) {
+    console.error(`Erro na leitura do arquivo ${error.message}`);
+  }
+};
+
+module.exports = {
+  readChocolates,
+};
